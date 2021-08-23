969b4a365082e9c187476ba16af4e75e1fbf3909
icaroM-CdC <icaro.moreira@protonmail.com>
2021-08-22 23:29:39 -0300

    b

---
+++ src/instructionsSB/instructionsSB.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstddef>
+#include <boost/convert.hpp>
+#include <bitset>
+#include <array>
+
+typedef struct SB_instruction SB_instruction;
+
+struct SB_instruction {
+
+};
+
+class SB_assembler {
+    
+};
