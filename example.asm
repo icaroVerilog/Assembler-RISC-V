ca7e530f802961753f756b124f3abe461f077093
Ícaro <icaro.moreira@protonmail.com>
2021-10-12 10:06:21 -0300

    fim

--- example.asm
+++ example.asm
@@ -1,11 +1,5 @@
-andi x2, x1, 16
-label1:
-    add x2, x0, x3
-    add x2, x0, x4
-    add x2, x0, x5
-label2:
-    add x2, x0, x11
-label3:
-    add x2, x0, x12
-    add x2, x0, x13
-    add x2, x0, x14
+add x3, x0, x2
+add x4, x2, x2
+add x5, x0, x2
+add x6, x3, x3
+add x4, x4, x2
