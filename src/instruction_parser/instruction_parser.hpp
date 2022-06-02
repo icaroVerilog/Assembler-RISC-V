af4231724972de1bd1e6dd02aaa06995d83ea71e
L3G4S0V <l3g4s0v@pop-os.localdomain>
2022-06-02 14:40:26 -0300

    update

---
+++ src/instruction_parser/instruction_parser.hpp
@@ -0,0 +1,19 @@
+#ifndef convert_operations
+#define convert_operations
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstddef>
+#include <boost/convert.hpp>
+#include <bitset>
+#include <regex>
+
+class Instruction_parser {
+    protected: I_instruction I_type_split (std::string& instruction);
+    protected: L_instruction L_type_split (std::string& instruction);
+    protected: R_instruction R_type_split (std::string& instruction);
+    protected: P_instruction P_type_split (std::string& instruction);
+};
+
+#endif /* convert_operations */
